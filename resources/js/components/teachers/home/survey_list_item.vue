f141dec156194f4e7eca8b7d093b801336faa15d
= <=>
2026-02-23 13:23:37 +0100

    la redirection depuis la page principale sur le bon sondage

--- resources/js/components/teachers/home/survey_list_item.vue
+++ resources/js/components/teachers/home/survey_list_item.vue
@@ -5,12 +5,16 @@ const props = defineProps({
         required: true
     }
 });
+
+const navigateToSetup = () => {
+    window.location.href = '/teachers/probe/set_up/' + props.survey.id;
+};
 </script>
 
 <template>
     <div class="bg-white rounded-lg shadow p-6">
         <h3 class="text-xl font-semibold mb-4">{{ props.survey.name }}</h3>
         <p class="text-gray-600 mb-4">Créé le {{ new Date(props.survey.created_at).toLocaleDateString() }}</p>
-        <button class="border rounded-lg p-2">Effectuer</button>
+        <button @click="navigateToSetup" class="border rounded-lg p-2">Effectuer</button>
     </div>
 </template>
---
+++ resources/js/pages/teachers/Probe/Set_up.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+
+const id = window.location.pathname.split('/')[4];
+
+function setUp() {
+    window.location.href = '/teachers/probe/display';
+}
+</script>
+
+<template>
+    <AppLayout>
+        <div class="flex-col items-center mb-10 mt-24">
+            <h2 class="text-2xl md:text-3xl font-bold">Enseignant - Sonder</h2>
+        </div>
+        <div>
+            <label for="class">Classe : </label>
+            <input type="text" id="class" name="class" class="border border-gray-300 rounded-md p-2 w-full">
+
+            <label for="remark">Remarque : </label>
+            <input type="text" id="remark" name="remark" class="border border-gray-300 rounded-md p-2 w-full">
+        </div>
+        <div class="mx-auto mt-40">
+            <button @click="setUp()"
+                class="bg-green-500 hover:bg-green-700 text-white font-bold px-12 py-6 rounded text-8xl">Valider</button>
+        </div>
+    </AppLayout>
+</template>
