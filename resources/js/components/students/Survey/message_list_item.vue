07253fb382800d624e94d648bafb8c48ad1551ee
= <=>
2026-02-27 13:38:33 +0100

    ajout de la page de sondage (étudiant)

---
+++ resources/js/components/students/Survey/message_list_item.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    message: Object
+});
+</script>
+<template>
+    <div class="flex items-center mb-4 space-x-4">
+        <span class="text-2xl">Message</span>
+        <button class="rounded-full bg-green-500 h-8 w-8">
+            <img src="/icons/like.png" class="p-2" alt="Icone de like">
+        </button>
+        <button class="rounded-full bg-red-500 h-8 w-8">
+            <img src="/icons/dislike.png" class="p-2" alt="Icone de dislike">
+        </button>
+    </div>
+</template>
--- resources/js/pages/students/Survey.vue
+++ resources/js/pages/students/Survey.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/layouts/AppLayout.vue';
+import MessageListItem from '@/components/students/Survey/message_list_item.vue';
 
 const props = defineProps({
     code: String
@@ -10,7 +11,33 @@ const props = defineProps({
     <AppLayout>
         <div class="flex-col items-center mb-10 mt-24">
             <h2 class="text-2xl md:text-3xl font-bold">Sondage</h2>
+
+            <MessageListItem></MessageListItem>
+
+            <div
+                class="fixed bottom-0 border-t border-gray-200 h-16 px-4 flex items-center gap-3">
+                <input type="text"
+                    class="flex-1 h-10 px-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Votre remarque...">
+
+                <div class="flex gap-2">
+                    <button
+                        class="rounded-full bg-green-500 hover:bg-green-600 h-10 w-10 transition">
+                        <img src="/icons/checkmark.png" class="p-2.5" alt="Validation">
+                    </button>
+                    <button
+                        class="rounded-full bg-red-500 hover:bg-red-600 h-10 w-10 transition">
+                        <img src="/icons/close.png" class="p-3" alt="Fermeture">
+                    </button>
+                    <button
+                        class="rounded-full bg-gray-400 hover:bg-gray-500 h-10 w-10 transition">
+                        <img src="/icons/lock.png" class="p-2" alt="Verrouillage">
+                    </button>
+                    <button class="ms-8 rounded-full hover:bg-gray-200 h-10 w-10 transition">
+                        <img src="/icons/message.png" class="p-2" alt="Message">
+                    </button>
+                </div>
+            </div>
         </div>
-        <h2>{{ props.code }}</h2>
     </AppLayout>
 </template>
